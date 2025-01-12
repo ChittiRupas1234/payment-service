6c9a7ad30ebbad6eaa821fa33b56094a957287a6
ChittiRupas1234 <139005647+ChittiRupas1234@users.noreply.github.com>
2025-01-12 16:14:19 +0530

    Payment service first commit (#2)
    * payment service first commit
    
    * Payment Service Application

--- src/main/java/com/payments/application/config/RestTemplateConfig.java
+++ src/main/java/com/payments/application/config/RestTemplateConfig.java
@@ -2,37 +2,6 @@ package com.payments.application.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpRequestExecution;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-
-import java.io.IOException;
-import java.util.Collections;
-
-@Configuration
-public class RestTemplateConfig {
-
-    @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setInterceptors(Collections.singletonList(new RequestHeaderModifierInterceptor()));
-        return restTemplate;
-    }
-
-    private static class RequestHeaderModifierInterceptor implements ClientHttpRequestInterceptor {
-        @Override
-        public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
-            // Modify request here if needed
-            return execution.execute(request, body);
-        }
-    }
-}
-
-/*
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -43,4 +12,3 @@ public class RestTemplateConfig {
         return new RestTemplate();
     }
 }
-*/
