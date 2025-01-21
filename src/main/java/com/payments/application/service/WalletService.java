177b726932d758ddb066095532a5de9d2cd0b38a
Chitti Rupas <chittirupasmaguluri@gmail.com>
2025-01-22 05:01:19 +0530

    Payment Service Application

--- src/main/java/com/payments/application/service/WalletService.java
+++ src/main/java/com/payments/application/service/WalletService.java
@@ -5,6 +5,7 @@ import com.payments.application.dto.TopupRequest;
 import com.payments.application.dto.TransferRequest;
 import com.payments.application.dto.WithdrawRequest;
 import com.payments.application.entity.Wallet;
+import com.payments.application.repository.WalletRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -47,6 +48,7 @@ public class WalletService {
         wallet.setBalance(wallet.getBalance().add(amount));
         Wallet updatedWallet = walletRepository.save(wallet);
 
+        kafkaTemplate.send("Topup", request);
         return updatedWallet;
     }
 
@@ -68,6 +70,7 @@ public class WalletService {
 
         // Publish the transfer request JSON to the "Transfer" topic
         kafkaTemplate.send("Transfer", request);
+
     }
 
     public BigDecimal getBalance(String token, UUID walletId) {
